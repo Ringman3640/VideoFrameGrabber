7ce030679ce2c76fcb7faa255f68c232ebe39006
Franz Alarcon <franzalarconv@gmail.com>
2024-12-15 02:51:58 -0800

    Add initial tests for constructor with PATH argument

--- tests/VideoFrameGrabber.Tests/FrameGrabberTests.cs
+++ tests/VideoFrameGrabber.Tests/FrameGrabberTests.cs
@@ -1,11 +1,44 @@
-﻿namespace VideoFrameGrabber.Tests
+﻿using FluentAssertions;
+using VideoFrameGrabber.Tests.CollectionFixtures;
+
+namespace VideoFrameGrabber.Tests
 {
+    [Collection("FFmpegDependency collection")]
     public class FrameGrabberTests
     {
-        [Fact]
-        public void Test1()
-        {
+        private FFmpegDependencyFixture ffmpegDependency;
 
+        public FrameGrabberTests(FFmpegDependencyFixture ffmpegFixture)
+        {
+            ffmpegDependency = ffmpegFixture;
+        }
+
+        [Fact]
+        public void Constructor_EnvPathArgument_Succeeds()
+        {
+            FrameGrabber? grabber = null;
+
+            try
+            {
+                grabber = new("ffmpeg");
+            }
+            catch { }
+
+            grabber.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void Constructor_NullArgument_Fails()
+        {
+            FrameGrabber? grabber = null;
+
+            try
+            {
+                grabber = new(null);
+            }
+            catch { }
+
+            grabber.Should().BeNull();
         }
     }
 }
