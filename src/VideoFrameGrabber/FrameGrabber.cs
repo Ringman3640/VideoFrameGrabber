309caa1a0eaf80700369f5077ad6898f61f2e73d
Franz Alarcon <franzalarconv@gmail.com>
2024-12-18 21:14:56 -0800

    Refactor constructor into helper methods

--- src/VideoFrameGrabber/FrameGrabber.cs
+++ src/VideoFrameGrabber/FrameGrabber.cs
@@ -20,79 +20,55 @@ namespace VideoFrameGrabber
                 throw new ArgumentException($"No FFmpeg path specified ({nameof(ffmpegPath)} was empty).");
             }
 
-            // Case: ffmpegPath specifies ffmpeg.exe is in PATH
-            if (ffmpegPath == "ffmpeg")
-            {
-                string? foundFFmpegFullPath = WinApiUtil.FindPathOfProgram("ffmpeg.exe");
-                if (foundFFmpegFullPath is null)
-                {
-                    throw new ArgumentException("Could not find ffmpeg.exe in PATH variable.");
-                }
-                ValidateFFmpegFile(foundFFmpegFullPath, nameof(ffmpegPath));
-                ffmpegLocation = foundFFmpegFullPath;
-                return;
-            }
+            string? confirmedFFmpegLocation = CheckFFmpegInEnvironmentPath(ffmpegPath)
+                ?? CheckFFmpegInDirectoryPath(ffmpegPath)
+                ?? CheckFFmpegFromFilePath(ffmpegPath)
+                ?? null;
 
-            FileAttributes ffmpegPathAttributes;
-            try
-            {
-                ffmpegPathAttributes = File.GetAttributes(ffmpegPath);
-            }
-            catch (Exception except)
-            {
-                FormatExceptionFromFileGetAttributes(except, nameof(ffmpegPath));
-
-                // Rethrow exception if FormatExceptionFromFileGetAttributes cannot reformat the
-                // exception.
-                // This will also guarantee to the compiler that ffmpegPathAttributes is initialized
-                // after exiting the try catch block
-                throw;
-            }
-
-            // Case: ffmpegPath specifies a directory with ffmpeg.exe
-            if (ffmpegPathAttributes.HasFlag(FileAttributes.Directory))
-            {
-                string ffmpegPathInDirectory = Path.GetFullPath(ffmpegPath + "./ffmpeg.exe");
-                if (!File.Exists(ffmpegPathInDirectory))
-                {
-                    throw new FileNotFoundException($"Folder path specified in {nameof(ffmpegPath)} does not contain ffmpeg.exe");
-                }
-                ValidateFFmpegFile(ffmpegPathInDirectory, nameof(ffmpegPath));
-                ffmpegLocation = ffmpegPathInDirectory;
-                return;
-            }
-
-            // Case: ffmpegPath is a direct path to an ffmpeg executable
-            string fullDirectFfmpegPath = Path.GetFullPath(ffmpegPath);
-            if (!File.Exists(fullDirectFfmpegPath))
+            if (confirmedFFmpegLocation == null)
             {
                 throw new FileNotFoundException($"Could not find the FFmpeg file specified in {nameof(ffmpegPath)}");
             }
-            ValidateFFmpegFile(fullDirectFfmpegPath, nameof(ffmpegPath));
-            ffmpegLocation = fullDirectFfmpegPath;
+
+            ValidateFFmpegFile(confirmedFFmpegLocation, nameof(ffmpegPath));
+            ffmpegLocation = Path.GetFullPath(confirmedFFmpegLocation);
         }
 
-        /// <summary>
-        /// Reformats an exception thrown by <see cref="File.GetAttributes(string)"/>.
-        /// </summary>
-        /// <param name="except">
-        /// The exception thrown by <see cref="File.GetAttributes(string)"/>.
-        /// </param>
-        /// <param name="parameterName">
-        /// Name of the method parameter that contains the exception-causing path.
-        /// </param>
-        /// <exception cref="FileNotFoundException"></exception>
-        /// <exception cref="UnauthorizedAccessException"></exception>
-        private void FormatExceptionFromFileGetAttributes(Exception except, string parameterName)
+        private string? CheckFFmpegInEnvironmentPath(string suggestedPath)
         {
-            if (except is FileNotFoundException)
+            if (suggestedPath != "ffmpeg")
             {
-                throw new FileNotFoundException($"Could not find a file at path specified in {parameterName}.");
+                return null;
             }
-            if (except is UnauthorizedAccessException)
+
+            string? foundFFmpegFullPath = WinApiUtil.FindPathOfProgram("ffmpeg.exe");
+            if (foundFFmpegFullPath is null)
             {
-                throw new UnauthorizedAccessException($"Caller does not have permission to acces the file specified in {parameterName}.");
+                throw new ArgumentException("Could not find ffmpeg.exe in PATH variable.");
             }
+
+            return foundFFmpegFullPath;
+        }
+
+        private string? CheckFFmpegInDirectoryPath(string suggestedPath)
+        {
+            if (!Directory.Exists(suggestedPath))
+            {
+                return null;
+            }
+
+            string ffmpegPathInDirectory = Path.GetFullPath(suggestedPath + "./ffmpeg.exe");
+            if (!File.Exists(ffmpegPathInDirectory))
+            {
+                throw new FileNotFoundException($"Folder path specified does not contain ffmpeg.exe");
+            }
+
+            return ffmpegPathInDirectory;
+        }
+
+        private string? CheckFFmpegFromFilePath(string suggestedPath)
+        {
+            return File.Exists(suggestedPath) ? suggestedPath : null;
         }
 
         /// <summary>
