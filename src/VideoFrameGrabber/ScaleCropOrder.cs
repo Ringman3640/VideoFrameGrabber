aeabb56289b0999092e629f07d4d47fa5c81cfc3
Franz Alarcon <franzalarconv@gmail.com>
2024-12-28 00:12:11 -0800

    Add ScaleCropOrder enum

---
+++ src/VideoFrameGrabber/ScaleCropOrder.cs
@@ -0,0 +1,18 @@
+﻿namespace VideoFrameGrabber
+{
+    /// <summary>
+    /// Specifies the order of scaling and cropping.
+    /// </summary>
+    public enum ScaleCropOrder
+    {
+        /// <summary>
+        /// Indicates scaling should be applied before cropping.
+        /// </summary>
+        ScaleFirst,
+
+        /// <summary>
+        /// Indicates cropping should be applied before scaling. 
+        /// </summary>
+        CropFirst
+    }
+}
