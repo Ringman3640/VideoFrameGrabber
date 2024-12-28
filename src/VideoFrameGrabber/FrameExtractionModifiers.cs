d80c5dbbf0c9430cf9d084c53c04e02da5e9117b
Franz Alarcon <franzalarconv@gmail.com>
2024-12-27 23:45:28 -0800

    Add IScaleProvider property to represent scaling

--- src/VideoFrameGrabber/FrameExtractionModifiers.cs
+++ src/VideoFrameGrabber/FrameExtractionModifiers.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using VideoFrameGrabber.Scaling;
 
 namespace VideoFrameGrabber
 {
     public class FrameExtractionModifiers
     {
         public TimeSpan SeekTime { get; set; }
+
+        public IScaleProvider? Scaling { get; set; }
     }
 }
