74d0765ba32eb30650824c416166157e29b8192d
Franz Alarcon <franzalarconv@gmail.com>
2024-12-28 20:34:23 -0800

    Add SizeScaler

---
+++ src/VideoFrameGrabber/Scaling/SizeScaler.cs
@@ -0,0 +1,42 @@
+﻿namespace VideoFrameGrabber.Scaling
+{
+    /// <summary>
+    /// Represents a scaler of constant size.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// Use this class to represent a known, constant width and height for a scaling operation.
+    /// </para>
+    /// <para>
+    /// This class is immutable.
+    /// </para>
+    /// </remarks>
+    public class SizeScaler : IScaleProvider
+    {
+        /// <summary>
+        /// Gets the pixel width component that this <see cref="SizeScaler"/> provides.
+        /// </summary>
+        public int Width { get; }
+        /// <summary>
+        /// Gets the pixel height component that this <see cref="SizeScaler"/> provides.
+        /// </summary>
+        public int Height { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SizeScaler"/> class with the specified
+        /// width and height.
+        /// </summary>
+        /// <param name="width">The pixel width of the scaler.</param>
+        /// <param name="height">The pixel height of the scaler.</param>
+        public SizeScaler(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public ScaleParameters GetScaleParameters(int inputWidth, int inputHeight)
+        {
+            return new ScaleParameters(Width, Height);
+        }
+    }
+}
---
+++ tests/VideoFrameGrabber.Tests/UnitTests/Scaling/SizeScalerUnitTests.cs
@@ -0,0 +1,82 @@
+﻿using FluentAssertions;
+using VideoFrameGrabber.Scaling;
+
+namespace VideoFrameGrabber.Tests.UnitTests.Scaling;
+
+public class SizeScalerUnitTests
+{
+    // T-1
+    [Theory]
+    [MemberData(nameof(GetWidthHeightValues))]
+    public void Constructor_SizeValues_WidthAndHeightMatchSizeValues(int width, int height)
+    {
+        SizeScaler? scaler = null;
+
+        scaler = new(width, height);
+
+        scaler.Should().NotBeNull();
+        scaler.Width.Should().Be(width);
+        scaler.Height.Should().Be(height);
+    }
+
+    // T-2
+    [Theory]
+    [MemberData(nameof(GetWidthHeightValues))]
+    public void GetScaleParameters_ZeroSize_ReturnedScaleParametersMatchConstructedSizeValues(int width, int height)
+    {
+        SizeScaler scaler = new(width, height);
+        ScaleParameters? scaleParameters = null;
+
+        scaleParameters = scaler.GetScaleParameters(0, 0);
+
+        scaleParameters.Should().NotBeNull();
+        scaleParameters.Value.Width.Should().Be(width);
+        scaleParameters.Value.Height.Should().Be(height);
+    }
+
+    // T-3
+    [Theory]
+    [MemberData(nameof(GetWidthHeightValues))]
+    public void GetScaleParameters_MaximumSize_ReturnedScaleParametersMatchConstructedSizeValues(int width, int height)
+    {
+        SizeScaler scaler = new(width, height);
+        ScaleParameters? scaleParameters = null;
+
+        scaleParameters = scaler.GetScaleParameters(int.MaxValue, int.MaxValue);
+
+        scaleParameters.Should().NotBeNull();
+        scaleParameters.Value.Width.Should().Be(width);
+        scaleParameters.Value.Height.Should().Be(height);
+    }
+
+    // T-4
+    [Theory]
+    [MemberData(nameof(GetWidthHeightValues))]
+    public void GetScaleParameters_MinimumSize_ReturnedScaleParametersMatchConstructedSizeValues(int width, int height)
+    {
+        SizeScaler scaler = new(width, height);
+        ScaleParameters? scaleParameters = null;
+
+        scaleParameters = scaler.GetScaleParameters(int.MinValue, int.MinValue);
+
+        scaleParameters.Should().NotBeNull();
+        scaleParameters.Value.Width.Should().Be(width);
+        scaleParameters.Value.Height.Should().Be(height);
+    }
+
+    /// <summary>
+    /// Gets test data that specifies width and height values.
+    /// </summary>
+    public static IEnumerable<object[]> GetWidthHeightValues()
+    {
+        yield return new object[] { 0, 0 };
+        yield return new object[] { 1, 1 };
+        yield return new object[] { 1000, 1000 };
+        yield return new object[] { -1, -1 };
+        yield return new object[] { -1000, -1000 };
+        yield return new object[] { int.MaxValue, int.MaxValue };
+        yield return new object[] { int.MinValue, int.MinValue };
+        yield return new object[] { int.MinValue, int.MaxValue };
+        yield return new object[] { int.MaxValue, int.MinValue };
+    }
+}
