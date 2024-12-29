d39c627d3dcafbcb8f4a81f86da9ab94c4b1dcec
Franz Alarcon <franzalarconv@gmail.com>
2024-12-28 22:21:08 -0800

    Add BoundsScaler

---
+++ src/VideoFrameGrabber/Scaling/BoundsScaler.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace VideoFrameGrabber.Scaling
+{
+    /// <summary>
+    /// Represents a scaler that limits scale sizes to a bounds size.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// The <see cref="BoundsScaler"/> only modifies the size of an image if the dimensions of the
+    /// image exceed the dimensions of its bounds size. If the image is larger that the bounds size,
+    /// the image will be scaled down to fit within the bounds.
+    /// </para>
+    /// <para>
+    /// If scaling occurs, <see cref="BoundsScaler"/> attempts to maintain the aspect ratio of the
+    /// image.
+    /// </para>
+    /// </remarks>
+    public class BoundsScaler : IScaleProvider
+    {
+        /// <summary>
+        /// Gets the pixel width of the bounds size.
+        /// </summary>
+        public int WidthBounds { get; }
+
+        /// <summary>
+        /// Gets the pixel height of the bounds size.
+        /// </summary>
+        public int HeightBounds { get; }
+
+        /// <summary>
+        /// Gets the aspect ratio of the bounds size. This is <see cref="WidthBounds"/> divided by
+        /// <see cref="HeightBounds"/>.
+        /// </summary>
+        private double BoundsAspectRatio { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BoundsScaler"/> class with the specified
+        /// bounds dimensions.
+        /// </summary>
+        /// <param name="widthBounds">The pixel width of the bounds.</param>
+        /// <param name="heightBounds">The pixel height of the bounds.</param>
+        /// <exception cref="ArgumentException">
+        /// <paramref name="widthBounds"/> or <paramref name="heightBounds"/> was zero or negative.
+        /// </exception>
+        /// <remarks>
+        /// The values for <paramref name="widthBounds"/> and <paramref name="heightBounds"/> must
+        /// be positive. This comes from the requirement for images to have a non-zero size.
+        /// </remarks>
+        public BoundsScaler(int widthBounds, int heightBounds)
+        {
+            if (widthBounds <= 0 || heightBounds <= 0)
+            {
+                throw new ArgumentException("Both bound values must be positive (not zero or negative).");
+            }
+
+            WidthBounds = widthBounds;
+            HeightBounds = heightBounds;
+            BoundsAspectRatio = widthBounds / heightBounds;
+        }
+
+        public ScaleParameters GetScaleParameters(int inputWidth, int inputHeight)
+        {
+            if (inputWidth <= WidthBounds && inputHeight <= HeightBounds)
+            {
+                // No shrinking needed, input is within bounds
+                return new ScaleParameters(inputWidth, inputHeight);
+            }
+
+            int outputWidth;
+            int outputHeight;
+            double inputAspectRatio = (double)inputWidth / inputHeight;
+            
+            if (inputAspectRatio > BoundsAspectRatio)
+            {
+                // Case: Input size is wider than bounds size, set input width to bounds width and
+                // adjust input height to keep aspect ratio
+                outputWidth = WidthBounds;
+                outputHeight = (int)(outputWidth / inputAspectRatio);
+            }
+            else if (inputAspectRatio < BoundsAspectRatio)
+            {
+                // Case: Iput size is taller than bounds size, set input height to bounds height and
+                // adjust input width to keep aspect ratio
+                outputHeight = HeightBounds;
+                outputWidth = (int)(outputHeight * inputAspectRatio);
+            }
+            else
+            {
+                // Case: Aspect ratios are the same, just shrink input size to bounds size
+                outputWidth = WidthBounds;
+                outputHeight = HeightBounds;
+            }
+
+            if (outputWidth <= 0)
+            {
+                outputWidth = 1;
+            }
+            if (outputHeight <= 0)
+            {
+                outputHeight = 1;
+            }
+
+            return new ScaleParameters(outputWidth, outputHeight);
+        }
+    }
+}
---
+++ tests/VideoFrameGrabber.Tests/UnitTests/Scaling/BoundsScalerUnitTests.cs
@@ -0,0 +1,111 @@
+﻿using FluentAssertions;
+using VideoFrameGrabber.Scaling;
+
+namespace VideoFrameGrabber.Tests.UnitTests.Scaling;
+
+public class BoundsScalerUnitTests
+{
+    // T-1
+    [Theory]
+    [InlineData(1, 1)]
+    [InlineData(100, 100)]
+    [InlineData(99999, 99999)]
+    [InlineData(int.MaxValue, int.MaxValue)]
+    public void Constructor_PositiveBoundsValues_WidthAndHeightBoundsMatchValues(
+        int width,
+        int height
+    ) {
+        BoundsScaler? boundsScaler = null;
+
+        try
+        {
+            boundsScaler = new(width, height);
+        }
+        catch { }
+
+        boundsScaler.Should().NotBeNull();
+        boundsScaler!.WidthBounds.Should().Be(width);
+        boundsScaler!.HeightBounds.Should().Be(height);
+    }
+
+    // T-2
+    [Theory]
+    [InlineData(0, 0)]
+    [InlineData(-100, -100)]
+    [InlineData(-1, -1)]
+    [InlineData(-99999, -99999)]
+    [InlineData(int.MinValue, int.MinValue)]
+    [InlineData(100, -100)]
+    [InlineData(1, -1)]
+    [InlineData(99999, -99999)]
+    [InlineData(int.MaxValue, int.MinValue)]
+    [InlineData(-100, 100)]
+    [InlineData(-1, 1)]
+    [InlineData(-99999, 99999)]
+    [InlineData(int.MinValue, int.MaxValue)]
+    public void Constructor_NegativeAndZeroBoundsValues_ThrowsArgumentException(
+        int width,
+        int height
+    )
+    {
+        BoundsScaler? boundsScaler = null;
+        Exception? exception = null;
+
+        try
+        {
+            boundsScaler = new(width, height);
+        }
+        catch (Exception except)
+        {
+            exception = except;
+        }
+
+        boundsScaler.Should().BeNull();
+        exception.Should().NotBeNull();
+        exception.Should().BeOfType<ArgumentException>();
+    }
+
+    // T-3
+    [Theory]
+    [MemberData(nameof(GetBoundsAndInputAndExpectedSizeValues))]
+    public void GetScaleParameters_BoundsSizeAndInputSizeValues_ResultMatchesExpectedDimensions(
+        Size boundsSize,
+        Size inputSize,
+        Size expectedSize
+    ) {
+        BoundsScaler boundsScaler = new(boundsSize.Width, boundsSize.Height);
+        ScaleParameters? scaleParameters = null;
+
+        try
+        {
+            scaleParameters = boundsScaler.GetScaleParameters(inputSize.Width, inputSize.Height);
+        }
+        catch { }
+
+        scaleParameters.Should().NotBeNull();
+        scaleParameters!.Value.Width.Should().Be(expectedSize.Width);
+        scaleParameters!.Value.Height.Should().Be(expectedSize.Height);
+    }
+
+    /// <summary>
+    /// Gets a set of trio <see cref="Size"/> values to test the
+    /// <see cref="BoundsScaler.GetScaleParameters(int, int)"/> method.
+    /// </summary>
+    public static IEnumerable<object[]> GetBoundsAndInputAndExpectedSizeValues()
+    {
+        int max = int.MaxValue;
+
+        yield return new object[] { new Size(100, 100), new Size(0, 0), new Size(0, 0) };
+        yield return new object[] { new Size(100, 100), new Size(10, 10), new Size(10, 10) };
+        yield return new object[] { new Size(100, 100), new Size(100, 100), new Size(100, 100) };
+        yield return new object[] { new Size(max, max), new Size(max, max), new Size(max, max) };
+        yield return new object[] { new Size(10, 10), new Size(100, 100), new Size(10, 10) };
+        yield return new object[] { new Size(10, 10), new Size(max, max), new Size(10, 10) };
+        yield return new object[] { new Size(100, 100), new Size(200, 100), new Size(100, 50) };
+        yield return new object[] { new Size(10, 10), new Size(100, 10), new Size(10, 1) };
+        yield return new object[] { new Size(1, 1), new Size(999, 1), new Size(1, 1) };
+        yield return new object[] { new Size(100, 100), new Size(100, 200), new Size(50, 100) };
+        yield return new object[] { new Size(10, 10), new Size(10, 100), new Size(1, 10) };
+        yield return new object[] { new Size(1, 1), new Size(1, 999), new Size(1, 1) };
+    }
+}
