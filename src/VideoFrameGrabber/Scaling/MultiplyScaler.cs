e3379eabd027cb00d863a27e8f166eeeadbfe8ca
Franz Alarcon <franzalarconv@gmail.com>
2024-12-28 20:34:42 -0800

    Add MultiplyScaler

---
+++ src/VideoFrameGrabber/Scaling/MultiplyScaler.cs
@@ -0,0 +1,65 @@
+﻿namespace VideoFrameGrabber.Scaling
+{
+    /// <summary>
+    /// Represents a scaler that multiplies the size of an image by a specified multiplier.
+    /// </summary>
+    public class MultiplyScaler : IScaleProvider
+    {
+        /// <summary>
+        /// Gets the multiplier value that is applied in <see cref="GetScaleParameters(int, int)"/>.
+        /// </summary>
+        public double Multiplier { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MultiplyScaler"/> class with the specified
+        /// multiplier value.
+        /// </summary>
+        /// <param name="multiplier">The multiplier value to apply for scaling operations.</param>
+        public MultiplyScaler(double multiplier)
+        {
+            Multiplier = multiplier;        
+        }
+
+        /// <remarks>
+        /// If the result of a multiply operation results in an overload, the result is capped to
+        /// <see cref="int.MaxValue"/> or <see cref="int.MinValue"/> (depending on the sign of
+        /// <see cref="Multiplier"/>. This is applied to both dimensions of the result
+        /// <see cref="ScaleParameters"/> independently, which may cause differences between the
+        /// input and output aspect ratio sizes if only one dimension overflows.
+        /// </remarks>
+        /// <inheritdoc cref="IScaleProvider.GetScaleParameters(int, int)"/>
+        public ScaleParameters GetScaleParameters(int inputWidth, int inputHeight)
+        {
+            double outputWidth;
+            try
+            {
+                checked
+                {
+                    outputWidth = inputWidth * Multiplier;
+                }
+            }
+            catch
+            {
+                outputWidth = Multiplier < 0 ? double.MinValue : double.MaxValue;
+            }
+
+            double outputHeight;
+            try
+            {
+                checked
+                {
+                    outputHeight = inputHeight * Multiplier;
+                }
+            }
+            catch
+            {
+                outputHeight = Multiplier < 0 ? double.MinValue : double.MaxValue;
+            }
+
+            return new ScaleParameters(
+                outputWidth > int.MaxValue ? int.MaxValue : (int)outputWidth,
+                outputHeight > int.MaxValue ? int.MaxValue : (int)outputHeight
+            );
+        }
+    }
+}
---
+++ tests/VideoFrameGrabber.Tests/UnitTests/Scaling/MultiplyScalerUnitTests.cs
@@ -0,0 +1,99 @@
+﻿using FluentAssertions;
+using VideoFrameGrabber.Scaling;
+
+namespace VideoFrameGrabber.Tests.UnitTests.Scaling;
+
+public class MultiplyScalerUnitTests
+{
+    [Theory]
+    [MemberData(nameof(GetMultiplierValues))]
+    public void Constructor_MultiplierValue_MultiplierMatchesValue(double multiplier)
+    {
+        MultiplyScaler? multiplyScaler = null;
+
+        multiplyScaler = new(multiplier);
+
+        multiplyScaler.Should().NotBeNull();
+        multiplyScaler.Multiplier.Should().Be(multiplier);
+    }
+
+    [Theory]
+    [MemberData(nameof(GetMultiplierAndSizeValues))]
+    public void GetScaleParameters_MultiplierAndInputSizeValues_ResultMatchesExpectedDimensions (
+        double multiplier,
+        Size inputSize,
+        Size expectedSize
+    ) {
+        MultiplyScaler multiplyScaler = new(multiplier);
+        ScaleParameters? scaleParameters = null;
+
+        scaleParameters = multiplyScaler.GetScaleParameters(inputSize.Width, inputSize.Height);
+
+        scaleParameters.Should().NotBeNull();
+        scaleParameters.Value.Width.Should().Be(expectedSize.Width);
+        scaleParameters.Value.Height.Should().Be(expectedSize.Height);
+    }
+
+    /// <summary>
+    /// Gets multiplier values of type <see cref="double"/>.
+    /// </summary>
+    public static IEnumerable<object[]> GetMultiplierValues()
+    {
+        yield return new object[] { 1d };
+        yield return new object[] { 10d };
+        yield return new object[] { 9999d };
+        yield return new object[] { 0.1d };
+        yield return new object[] { 0.001d };
+        yield return new object[] { 0.9d };
+        yield return new object[] { 0d };
+        yield return new object[] { double.MaxValue };
+        yield return new object[] { -1d };
+        yield return new object[] { -10d };
+        yield return new object[] { -9999d };
+        yield return new object[] { -0.1d };
+        yield return new object[] { -0.001d };
+        yield return new object[] { -0.9d };
+        yield return new object[] { double.MinValue };
+    }
+
+    /// <summary>
+    /// Gets multiplier values of type <see cref="double"/> and gets input and expected
+    /// <see cref="Size"/> dimensions.
+    /// </summary>
+    public static IEnumerable<object[]> GetMultiplierAndSizeValues()
+    {
+        Size maxSizeBox = new(int.MaxValue, int.MaxValue);
+        Size zeroSize = new(0, 0);
+
+        yield return new object[] { 1d, new Size(100, 100), new Size(100, 100) };
+        yield return new object[] { 2d, new Size(100, 100), new Size(200, 200) };
+        yield return new object[] { 0.5d, new Size(100, 100), new Size(50, 50) };
+        yield return new object[] { 0.1d, new Size(100, 100), new Size(10, 10) };
+        yield return new object[] { 0, new Size(100, 100), zeroSize };
+        yield return new object[] { 1d, zeroSize, zeroSize };
+        yield return new object[] { 999d, zeroSize, zeroSize };
+        yield return new object[] { double.MaxValue, zeroSize, zeroSize };
+        yield return new object[] { double.MaxValue, new Size(100, 100), maxSizeBox };
+        yield return new object[] { double.MaxValue, maxSizeBox, maxSizeBox };
+        yield return new object[] { 2d, maxSizeBox, maxSizeBox };
+    }
+
+    /// <summary>
+    /// Represents a width and height size.
+    /// </summary>
+    /// <remarks>
+    /// This struct serves as syntax above raw width and height values. It makes the test code
+    /// easier to understand by grouping corresponding width and height values.
+    /// </remarks>
+    public readonly struct Size
+    {
+        public int Width { get; init; }
+        public int Height { get; init; }
+
+        public Size(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+    }
+}
