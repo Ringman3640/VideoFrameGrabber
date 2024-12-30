3af4ba178c3d73bdff1eee1a5bf816996487c1a1
Franz Alarcon <franzalarconv@gmail.com>
2024-12-30 00:55:09 -0800

    Add HeightScaler

---
+++ src/VideoFrameGrabber/Scaling/HeightScaler.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace VideoFrameGrabber.Scaling
+{
+    /// <summary>
+    /// Represents a scaler that sets the height of an image to a specific value while attempting to
+    /// maintain the image's aspect ratio.
+    /// </summary>
+    /// <remarks>
+    /// <see cref="HeightScaler"/> attempts to maintain the aspect ratio of the image while applying
+    /// scaling values. However, this may fail if the width of the image is larger that its height
+    /// and <see cref="HeightScaler"/> scales the image height to a value close to
+    /// <see cref="int.MaxValue"/>. Since a dimension of an image cannot exceed
+    /// <see cref="int.MaxValue"/>, the width value would be clamped down in this scenario.
+    /// </remarks>
+    public class HeightScaler
+    {
+        /// <summary>
+        /// Gets the height pixel length that will be applied to the image.
+        /// </summary>
+        public int Height { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HeightScaler"/> class with the specified
+        /// height length.
+        /// </summary>
+        /// <param name="length">Pixel length of the height value.</param>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// <paramref name="length"/> was zero or negative.
+        /// </exception>
+        public HeightScaler(int length)
+        {
+            if (length <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(length));
+            }
+
+            Height = length;
+        }
+
+        public ScaleParameters GetScaleParameters(int inputWidth, int inputHeight)
+        {
+            double outWidth;
+            double widthMultiplier = (double)inputWidth / inputHeight;
+
+            try
+            {
+                checked
+                {
+                    outWidth = Height * widthMultiplier;
+                }
+            }
+            catch
+            {
+                outWidth = int.MaxValue;
+            }
+
+            return new ScaleParameters(
+                outWidth > int.MaxValue ? int.MaxValue : (int)outWidth,
+                Height
+            );
+        }
+    }
+}
---
+++ tests/VideoFrameGrabber.Tests/UnitTests/Scaling/HeightScalerUnitTests.cs
@@ -0,0 +1,97 @@
+﻿using FluentAssertions;
+using VideoFrameGrabber.Scaling;
+
+namespace VideoFrameGrabber.Tests.UnitTests.Scaling;
+
+public  class HeightScalerUnitTests
+{
+    [Theory]
+    [InlineData(1)]
+    [InlineData(100)]
+    [InlineData(99999)]
+    [InlineData(int.MaxValue)]
+    public void Constructor_ValidHeightValue_HeightMatchesValue(int height)
+    {
+        HeightScaler? scaler = null;
+
+        try
+        {
+            scaler = new(height);
+        }
+        catch { }
+
+        scaler.Should().NotBeNull();
+        scaler!.Height.Should().Be(height);
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    [InlineData(-100)]
+    [InlineData(-99999)]
+    [InlineData(int.MinValue)]
+    public void Constructor_ZeroOrNegativeValue_ThrowsArgumentOutOfRangeException(int height)
+    {
+        HeightScaler? scaler = null;
+        Exception? exception = null;
+
+        try
+        {
+            scaler = new(height);
+        }
+        catch (Exception except)
+        {
+            exception = except;
+        }
+
+        scaler.Should().BeNull();
+        exception.Should().NotBeNull();
+        exception.Should().BeOfType<ArgumentOutOfRangeException>();
+    }
+
+    [Theory]
+    [MemberData(nameof(GetHeightAndInputAndExpectedValues))]
+    public void GetScaleParameters_HeightAndInputSizeValues_ResultMatchesExpectedSize(
+        int height,
+        Size inputSize,
+        Size expectedSize
+    )
+    {
+        HeightScaler scaler = new(height);
+        ScaleParameters? scaleParameters = null;
+
+        try
+        {
+            scaleParameters = scaler.GetScaleParameters(inputSize.Width, inputSize.Height);
+        }
+        catch { }
+
+        scaleParameters.Should().NotBeNull();
+        scaleParameters!.Value.Width.Should().Be(expectedSize.Width);
+        scaleParameters!.Value.Height.Should().Be(expectedSize.Height);
+    }
+
+    /// <summary>
+    /// Gets a height value, an input <see cref="Size"/> value, and an expected <see cref="Size"/>
+    /// value. Use to test <see cref="HeightScaler.GetScaleParameters(int, int)"/>.
+    /// </summary>
+    public static IEnumerable<object[]> GetHeightAndInputAndExpectedValues()
+    {
+        int max = int.MaxValue;
+
+        yield return new object[] { 100, new Size(1, 100), new Size(1, 100) };
+        yield return new object[] { 100, new Size(50, 100), new Size(50, 100) };
+        yield return new object[] { 100, new Size(50, 50), new Size(100, 100) };
+        yield return new object[] { 100, new Size(50, 10), new Size(500, 100) };
+        yield return new object[] { 100, new Size(50, 1), new Size(5000, 100) };
+        yield return new object[] { 1, new Size(10, 10), new Size(1, 1) };
+        yield return new object[] { 1, new Size(50, 10), new Size(5, 1) };
+        yield return new object[] { 1, new Size(5000, 10), new Size(500, 1) };
+        yield return new object[] { 1, new Size(max, max), new Size(1, 1) };
+        yield return new object[] { max, new Size(1, 1), new Size(max, max) };
+        yield return new object[] { max, new Size(max, max), new Size(max, max) };
+        yield return new object[] { max, new Size(max, 1), new Size(max, max) };
+        yield return new object[] { max, new Size(max, 100), new Size(max, max) };
+        yield return new object[] { max, new Size(max, 999999), new Size(max, max) };
+    }
+}
