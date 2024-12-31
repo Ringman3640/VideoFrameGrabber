18d5d9bc47bdb8d3953f8ad2fd973cbcf0e48b87
Franz Alarcon <franzalarconv@gmail.com>
2024-12-30 20:36:10 -0800

    Add CustomScaler

---
+++ src/VideoFrameGrabber/Scaling/CustomScaler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace VideoFrameGrabber.Scaling
+{
+    /// <summary>
+    /// Represents a scaler that uses a custom scaling function to produce a scale value.
+    /// </summary>
+    public class CustomScaler : IScaleProvider
+    {
+        /// <summary>
+        /// Gets the user-defined function that performs the scaling operation.
+        /// </summary>
+        public Func<int, int, ScaleParameters> ScaleFunction { get; }
+
+        /// <summary>
+        /// Initializes a new intance of the <see cref="CustomScaler"/> class with the specified
+        /// scaler function.
+        /// </summary>
+        /// <param name="scaleFunction">A user-defined function that provides a scale value.</param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="scaleFunction"/> was <c>null</c>.
+        /// </exception>
+        public CustomScaler(Func<int, int, ScaleParameters> scaleFunction)
+        {
+            if (scaleFunction == null)
+            {
+                throw new ArgumentNullException(nameof(scaleFunction));
+            }
+
+            ScaleFunction = scaleFunction;
+        }
+
+        public ScaleParameters GetScaleParameters(int inputWidth, int inputHeight)
+        {
+            ScaleParameters result = ScaleFunction(inputWidth, inputHeight);
+            return new ScaleParameters(
+                result.Width <= 0 ? 1 : result.Width,
+                result.Height <= 0 ? 1 : result.Height
+            );
+        }
+    }
+}
---
+++ tests/VideoFrameGrabber.Tests/UnitTests/Scaling/CustomScalerUnitTests.cs
@@ -0,0 +1,136 @@
+﻿using FluentAssertions;
+using VideoFrameGrabber.Scaling;
+
+namespace VideoFrameGrabber.Tests.UnitTests.Scaling;
+
+public class CustomScalerUnitTests
+{
+    // T-1
+    [Theory]
+    [MemberData(nameof(GetScaleFunctionValues))]
+    public void Constructor_ValidScaleFunction_Succeeds(Func<int, int, ScaleParameters> scaleFunction)
+    {
+        CustomScaler? scaler = null;
+
+        try
+        {
+            scaler = new(scaleFunction);
+        }
+        catch { }
+
+        scaler.Should().NotBeNull();
+    }
+
+    // T-2
+    [Fact]
+    public void Constructor_NullValue_ThrowsArgumentNullException()
+    {
+        CustomScaler? scaler = null;
+        Exception? exception = null;
+
+        try
+        {
+            scaler = new(null!);
+        }
+        catch (Exception except)
+        {
+            exception = except;
+        }
+
+        scaler.Should().BeNull();
+        exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+    }
+
+    // T-3
+    [Theory]
+    [MemberData(nameof(GetScaleFunctionValues))]
+    public void GetScaleParameters_ScaleFunctionAndSquareInputSizeValues_ResultMatchesResultFromScaleFunction(
+        Func<int, int, ScaleParameters> scaleFunction
+    ) {
+        int inputWidth = 100;
+        int inputHeight = 100;
+        CustomScaler scaler = new(scaleFunction);
+        ScaleParameters expectedResult = scaleFunction(inputWidth, inputHeight);
+        ScaleParameters? scaleParameters = null;
+
+        try
+        {
+            scaleParameters = scaler.GetScaleParameters(inputWidth, inputHeight);
+        }
+        catch { }
+
+        scaleParameters.Should().NotBeNull();
+        scaleParameters!.Value.Width.Should().Be(expectedResult.Width);
+        scaleParameters!.Value.Height.Should().Be(expectedResult.Height);
+    }
+
+    [Fact]
+    public void GetScaleParameters_SquareInputSizeWithScaleFunctionReturningZeroScale_ReturnsOneByOneScale()
+    {
+        int inputWidth = 100;
+        int inputHeight = 100;
+        CustomScaler scaler = new((inWidth, inHeight) => new ScaleParameters(0, 0));
+        ScaleParameters? scaleParameters = null;
+
+        try
+        {
+            scaleParameters = scaler.GetScaleParameters(inputWidth, inputHeight);
+        }
+        catch { }
+
+        scaleParameters.Should().NotBeNull();
+        scaleParameters!.Value.Width.Should().Be(1);
+        scaleParameters!.Value.Height.Should().Be(1);
+    }
+
+    [Fact]
+    public void GetScaleParameters_SquareInputSizeWithScaleFunctionReturningNegativeScale_ReturnsOneByOneScale()
+    {
+        int inputWidth = 100;
+        int inputHeight = 100;
+        CustomScaler scaler = new((inWidth, inHeight) => new ScaleParameters(-1, -1));
+        ScaleParameters? scaleParameters = null;
+
+        try
+        {
+            scaleParameters = scaler.GetScaleParameters(inputWidth, inputHeight);
+        }
+        catch { }
+
+        scaleParameters.Should().NotBeNull();
+        scaleParameters!.Value.Width.Should().Be(1);
+        scaleParameters!.Value.Height.Should().Be(1);
+    }
+
+    /// <summary>
+    /// Gets scale provider callback methods. Use to test
+    /// <see cref="CustomScaler.CustomScaler(Func{int, int, ScaleParameters})"/>.
+    /// </summary>
+    public static IEnumerable<object[]> GetScaleFunctionValues()
+    {
+        yield return new object[] 
+        {
+            (int inWidth, int inHeight) =>
+            {
+                return new ScaleParameters(0, 0);
+            }
+        };
+
+        yield return new object[]
+        {
+            (int inWidth, int inHeight) =>
+            {
+                return new ScaleParameters(inWidth, inHeight);
+            }
+        };
+
+        yield return new object[]
+        {
+            (int inWidth, int inHeight) =>
+            {
+                FitBoundsScaler fitBoundsScaler = new(100, 100);
+                return fitBoundsScaler.GetScaleParameters(inWidth, inHeight);
+            }
+        };
+    }
+}
