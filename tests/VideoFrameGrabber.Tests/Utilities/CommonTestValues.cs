91eff9ac47bdee26fb90422797fb87f63d42316a
Franz Alarcon <franzalarconv@gmail.com>
2025-01-05 03:53:49 -0800

    Add Append subclass; refactor SingleInts subclass

--- tests/VideoFrameGrabber.Tests/Utilities/CommonTestValues.cs
+++ tests/VideoFrameGrabber.Tests/Utilities/CommonTestValues.cs
@@ -3,7 +3,7 @@
 namespace VideoFrameGrabber.Tests.Utilities;
 
 /// <summary>
-/// Exposes classes that provide common test values.
+/// Exposes classes that provide common test values for xUnit theory tests.
 /// </summary>
 public static class CommonTestValues
 {
@@ -13,39 +13,60 @@ public static class CommonTestValues
     public static class SingleInts
     {
         /// <summary>
+        /// Gets a list of common positive <see cref="int"/> test values (not including zero).
+        /// </summary>
+        public static List<int> PositiveIntDataSet { get; } = [
+            1,
+            2,
+            10,
+            1000,
+            9999999,
+            int.MaxValue - 1,
+            int.MaxValue
+        ];
+
+        /// <summary>
+        /// Gets a list of common negative <see cref="int"/> test values (not including zero).
+        /// </summary>
+        public static List<int> NegativeIntDataSet { get; } = [
+            1,
+            2,
+            10,
+            1000,
+            9999999,
+            int.MaxValue - 1,
+            int.MaxValue
+        ];
+
+        /// <summary>
         /// Represents test data consisting of only positive integer values. This does not include
-        /// the zero value.
+        /// the zero value. Values are provided by <see cref="PositiveIntDataSet"/>.
         /// </summary>
         public class AllPositive : IEnumerable<object[]>
         {
             public IEnumerator<object[]> GetEnumerator()
             {
-                yield return new object[] { 1 };
-                yield return new object[] { 2 };
-                yield return new object[] { 10 };
-                yield return new object[] { 1000 };
-                yield return new object[] { 99999999 };
-                yield return new object[] { int.MaxValue - 1 };
-                yield return new object[] { int.MaxValue };
+                foreach (int value in PositiveIntDataSet)
+                {
+                    yield return new object[] { value };
+                }
             }
 
             IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
         }
 
         /// <summary>
-        /// Represents test data consisting of negative integer values.
+        /// Represents test data consisting of negative integer values. Values are provided by
+        /// <see cref="NegativeIntDataSet"/>.
         /// </summary>
         public class AllNegative : IEnumerable<object[]>
         {
             public IEnumerator<object[]> GetEnumerator()
             {
-                yield return new object[] { -1 };
-                yield return new object[] { -2 };
-                yield return new object[] { -10 };
-                yield return new object[] { -1000 };
-                yield return new object[] { -99999999 };
-                yield return new object[] { int.MinValue + 1 };
-                yield return new object[] { int.MinValue };
+                foreach (int value in NegativeIntDataSet)
+                {
+                    yield return new object[] { value };
+                }
             }
 
             IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
@@ -89,6 +110,26 @@ public static class CommonTestValues
         }
 
         /// <summary>
+        /// Represents a smaller set of test data (compared to <see cref="AllPositive"/>) consisting
+        /// of only positive integer pairs (including non-zero).
+        /// </summary>
+        public class SmallerAllPositive : IEnumerable<object[]>
+        {
+            public IEnumerator<object[]> GetEnumerator()
+            {
+                yield return new object[] { 1, 1 };
+                yield return new object[] { 1, 99999999 };
+                yield return new object[] { 1, int.MaxValue };
+                yield return new object[] { 99999999, 1 };
+                yield return new object[] { int.MaxValue, 1 };
+                yield return new object[] { 99999999, 99999999 };
+                yield return new object[] { int.MaxValue, int.MaxValue };
+            }
+
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+
+        /// <summary>
         /// Represents test data consisting of interger pairs with one or more zero values.
         /// </summary>
         public class ContainsZero : IEnumerable<object[]>
@@ -108,6 +149,26 @@ public static class CommonTestValues
                 yield return new object[] { 1000, 0 };
                 yield return new object[] { 99999999, 0 };
                 yield return new object[] { int.MaxValue - 1, 0 };
+                yield return new object[] { int.MaxValue, 0 };
+            }
+
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+
+        /// <summary>
+        /// Represents a smaller set of test data (compared to <see cref="ContainsZero"/>)
+        /// consisting of interger pairs with one or more zero values.
+        /// </summary>
+        public class SmallerContainsZero : IEnumerable<object[]>
+        {
+            public IEnumerator<object[]> GetEnumerator()
+            {
+                yield return new object[] { 0, 0 };
+                yield return new object[] { 0, 1 };
+                yield return new object[] { 0, 99999999 };
+                yield return new object[] { 0, int.MaxValue };
+                yield return new object[] { 1, 0 };
+                yield return new object[] { 99999999, 0 };
                 yield return new object[] { int.MaxValue, 0 };
             }
 
@@ -143,6 +204,61 @@ public static class CommonTestValues
             }
 
             IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+
+        /// <summary>
+        /// Represents a smaller set of test data (compared to <see cref="ContainsNegatives"/>)
+        /// consisting of integer pairs with one or more negative values.
+        /// </summary>
+        public class SmallerContainsNegatives : IEnumerable<object[]>
+        {
+            public IEnumerator<object[]> GetEnumerator()
+            {
+                yield return new object[] { 1, -1 };
+                yield return new object[] { 1, -99999999 };
+                yield return new object[] { 1, int.MinValue };
+                yield return new object[] { -1, 1 };
+                yield return new object[] { -99999999, 1 };
+                yield return new object[] { int.MinValue, 1 };
+                yield return new object[] { -1, -1 };
+                yield return new object[] { -99999999, -99999999 };
+                yield return new object[] { int.MinValue, int.MinValue };
+            }
+
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+    }
+
+    /// <summary>
+    /// Exposes methods that append pre-defined test values to a given value set.
+    /// </summary>
+    public static class Append
+    {
+        public static IEnumerable<object[]> PositiveIntPairs(object[] originalSet)
+        {
+            foreach (object[] positivePair in new IntPairs.SmallerAllPositive())
+            {
+                List<object> objectList = [.. originalSet, .. positivePair];
+                yield return objectList.ToArray();
+            }
+        }
+
+        public static IEnumerable<object[]> IntPairsContainingZero(object[] originalSet)
+        {
+            foreach (object[] zeroPairs in new IntPairs.SmallerContainsZero())
+            {
+                List<object> objectList = [.. originalSet, .. zeroPairs];
+                yield return objectList.ToArray();
+            }
+        }
+
+        public static IEnumerable<object[]> IntPairsContainingNegatives(object[] originalSet)
+        {
+            foreach (object[] negativePairs in new IntPairs.SmallerContainsNegatives())
+            {
+                List<object> objectList = [.. originalSet, .. negativePairs];
+                yield return objectList.ToArray();
+            }
         }
     }
 }
