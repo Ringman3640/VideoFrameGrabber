b684e12cd7b1965dcb2bcdbd66da7a0ba9bc3354
Franz Alarcon <franzalarconv@gmail.com>
2025-01-21 00:13:12 -0800

    Complete implementation

--- src/VideoFrameGrabber/Cropping/SizeCropper.cs
+++ src/VideoFrameGrabber/Cropping/SizeCropper.cs
@@ -1,31 +1,91 @@
-﻿namespace VideoFrameGrabber.Cropping
+﻿using System;
+
+namespace VideoFrameGrabber.Cropping
 {
     public class SizeCropper : CropProvider
     {
         public int Width { get; }
+
         public int Height { get; }
+
         public int X { get; }
+
         public int Y { get; }
+
         public CropAlign Align { get; }
 
         public SizeCropper(int width, int height)
         {
-            throw new System.NotImplementedException();
+            if (width < MinimumCropDimensionSize)
+            {
+                throw new ArgumentOutOfRangeException(nameof(width));
+            }
+            if (height < MinimumCropDimensionSize)
+            {
+                throw new ArgumentOutOfRangeException(nameof(height));
+            }
+
+            Width = width;
+            Height = height;
+            X = 0;
+            Y = 0;
+            Align = CropAlign.None;
         }
 
         public SizeCropper(int width, int height, int x, int y)
         {
-            throw new System.NotImplementedException();
+            if (width < MinimumCropDimensionSize)
+            {
+                throw new ArgumentOutOfRangeException(nameof(width));
+            }
+            if (height < MinimumCropDimensionSize)
+            {
+                throw new ArgumentOutOfRangeException(nameof(height));
+            }
+
+            Width = width;
+            Height = height;
+            X = x;
+            Y = y;
+            Align = CropAlign.None;
         }
 
         public SizeCropper(int width, int height, CropAlign align)
         {
-            throw new System.NotImplementedException();
+            if (width < MinimumCropDimensionSize)
+            {
+                throw new ArgumentOutOfRangeException(nameof(width));
+            }
+            if (height < MinimumCropDimensionSize)
+            {
+                throw new ArgumentOutOfRangeException(nameof(height));
+            }
+
+            Width = width;
+            Height = height;
+            X = 0;
+            Y = 0;
+            Align = align;
         }
 
         protected override void PerformCrop(ref int width, ref int height, ref int x, ref int y)
         {
-            throw new System.NotImplementedException();
+            if (Align == CropAlign.None)
+            {
+                width = Width;
+                height = Height;
+                x = X;
+                y = Y;
+            }
+            else
+            {
+                CropOffset offset = GetCropOffsetFromAlign(Width, Height, width, height, Align);
+
+                width = Width;
+                height = Height;
+                x = offset.X;
+                y = offset.Y;
+            }
         }
     }
 }
