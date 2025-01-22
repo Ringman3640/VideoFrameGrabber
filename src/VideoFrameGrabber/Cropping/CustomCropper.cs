63ecbf0a6ce4426f0cb2ac5d4a62af003491b89c
Franz Alarcon <franzalarconv@gmail.com>
2025-01-22 00:08:22 -0800

    Add CustomCropper interface

---
+++ src/VideoFrameGrabber/Cropping/CustomCropper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace VideoFrameGrabber.Cropping
+{
+    /// <summary>
+    /// Represents a <see cref="CropProvider"/> with custom cropping function to produce a crop
+    /// result.
+    /// </summary>
+    public class CustomCropper : CropProvider
+    {
+        /// <summary>
+        /// Gets the cropping function used to produce a crop result.
+        /// </summary>
+        public Func<int, int, CropParameters> CropFunction { get; }
+
+        /// <summary>
+        /// Initializes a new intance of the <see cref="CustomCropper"/> class with the specified
+        /// crop function.
+        /// </summary>
+        /// <param name="cropFunction">
+        /// A user-defined function that provides a <see cref="CropParameters"/> value.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="cropFunction"/> was <c>null</c>.
+        /// </exception>
+        public CustomCropper(Func<int, int, CropParameters> cropFunction)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void PerformCrop(ref int width, ref int height, ref int x, ref int y)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
