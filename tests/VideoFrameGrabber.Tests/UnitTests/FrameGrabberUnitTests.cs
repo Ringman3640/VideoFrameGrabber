a597d068b0f7ab156781314a45d577a2f4a491d8
Franz Alarcon <franzalarconv@gmail.com>
2025-03-08 19:36:43 -0800

    Add tests for GetFFmpegArgs methods

--- tests/VideoFrameGrabber.Tests/UnitTests/FrameGrabberUnitTests.cs
+++ tests/VideoFrameGrabber.Tests/UnitTests/FrameGrabberUnitTests.cs
@@ -69,4 +69,77 @@ public class FrameGrabberUnitTests
 
         result.Should().BeEquivalentTo(expected);
     }
+
+    /// <summary>
+    /// Tests the validity of
+    /// <see cref="FrameGrabber.FFmpegFormatting.GetFFmpegArgs(TimeSpan, string, string, ImageFormat)"/>.
+    /// </summary>
+    [Fact]
+    public void GetFFmpegArgs_SeekTimeAndVideoPathAndFiltersAndImageFormatArguments_ReturnsExpectedArgs() {
+        TimeSpan seekTime = TimeSpan.Parse("00:00:30");
+        string videoPath = "./videos/super_cool_video.mp4";
+        string filters = "scale=150:150";
+        ImageFormat imageFormat = ImageFormat.Jpg;
+        string expectedArgs = "-ss 30 -i \"./videos/super_cool_video.mp4\" -v:f \"scale=150:150\" -vframes 1 -f image2pipe -c:v mjpeg -";
+        string receivedArgs;
+
+        receivedArgs = FrameGrabber.FFmpegFormatting.GetFFmpegArgs(seekTime, videoPath, filters, imageFormat);
+
+        receivedArgs.Should().BeEquivalentTo(expectedArgs);
+    }
+
+    /// <summary>
+    /// Tests the validity of
+    /// <see cref="FrameGrabber.FFmpegFormatting.GetFFmpegArgs(TimeSpan, string, string, string)"/>.
+    /// </summary>
+    [Fact]
+    public void GetFFmpegArgs_SeekTimeAndVideoPathAndFiltersAndOutputPathArguments_ReturnsExpectedArgs()
+    {
+        TimeSpan seekTime = TimeSpan.Parse("00:00:05.5");
+        string videoPath = "./videos/super_lame_video.mp4";
+        string filters = "scale=600:0,crop=200:200";
+        string outputPath = "./thumbs/thumb.png";
+        string expectedArgs = "-ss 5.5 -i \"./videos/super_lame_video.mp4\" -v:f \"scale=600:0,crop=200:200\" -vframes 1 \"./thumbs/thumb.png\"";
+        string receivedArgs;
+
+        receivedArgs = FrameGrabber.FFmpegFormatting.GetFFmpegArgs(seekTime, videoPath, filters, outputPath);
+
+        receivedArgs.Should().BeEquivalentTo(expectedArgs);
+    }
+
+    /// <summary>
+    /// Tests the validity of
+    /// <see cref="FrameGrabber.FFmpegFormatting.GetFFmpegArgs(string, string, ImageFormat)"/>.
+    /// </summary>
+    [Fact]
+    public void GetFFmpegArgs_VideoPathAndFiltersAndImageFormatArguments_ReturnsExpectedArgs()
+    {
+        string videoPath = "./videos/insane_switch_axe_clip.mp4";
+        string filters = "crop=100:100:0:0";
+        ImageFormat imageFormat = ImageFormat.Bmp;
+        string expectedArgs = "-sseof -3 -i \"./videos/insane_switch_axe_clip.mp4\" -update 1 -v:f \"crop=100:100:0:0\" -f image2pipe -c:v bmp -";
+        string receivedArgs;
+
+        receivedArgs = FrameGrabber.FFmpegFormatting.GetFFmpegArgs(videoPath, filters, imageFormat);
+
+        receivedArgs.Should().BeEquivalentTo(expectedArgs);
+    }
+
+    /// <summary>
+    /// Tests the validity of
+    /// <see cref="FrameGrabber.FFmpegFormatting.GetFFmpegArgs(string, string, string)"/>.
+    /// </summary>
+    [Fact]
+    public void GetFFmpegArgs_VideoPathAndFiltersAndOutputPathArguments_ReturnsExpectedArgs()
+    {
+        string videoPath = "./videos/insane_charge_blade_clip.mov";
+        string filters = "scale=500:0";
+        string outputPath = "./thumbs/bruh.gif";
+        string expectedArgs = "-sseof -3 -i \"./videos/insane_charge_blade_clip.mov\" -update 1 -v:f \"scale=500:0\" \"./thumbs/bruh.gif\"";
+        string receivedArgs;
+
+        receivedArgs = FrameGrabber.FFmpegFormatting.GetFFmpegArgs(videoPath, filters, outputPath);
+
+        receivedArgs.Should().BeEquivalentTo(expectedArgs);
+    }
 }
